bbaec26ffb0bebd19dddefa63cc580fdb2f6f3f1
leo12appelt <167657728+leo12appelt@users.noreply.github.com>
2026-04-26 17:51:59 -0400

    calculadora V1
    primeira versão
    futuramente será um calculadora científica

---
+++ script.js
@@ -0,0 +1,51 @@
+let display ='';
+
+//processa a expreção para uma versao mais usual pra quem usa
+//só usada para mostar no display
+function processada(a) {
+    a = String(a)
+    let resultado = a.replaceAll('.',',').replaceAll('*','X').replaceAll('/','÷');
+    
+    return resultado == "67" ? "67🤚🖐️" : resultado;
+}
+
+//adiciona coisas no display
+function add(a) {
+    display += a;
+    
+    document.getElementById('display').value = processada(display);
+}
+
+//limpa o display
+function limpar() {
+    display = "";
+    document.getElementById('display').value = display;
+}
+
+//apaga o ultimo caractere
+function apagarCaractere() {
+    display = display.slice(0, -1);
+    document.getElementById('display').value = processada(display);
+}
+
+//salvar e colar
+let salvo;
+function salvar() {
+        salvo = eval(display);
+        salvo = string(salvo)
+}
+function colar() {
+    add(salvo);
+}
+
+//calcula e exibe
+function calcular() {
+    try{
+    display = eval(display);
+    display = String(display)
+    document.getElementById('display').value = processada(display);
+    }catch{  //em caso de erro 
+        display = '';
+        document.getElementById('display').value = "erro"
+    }
+}
